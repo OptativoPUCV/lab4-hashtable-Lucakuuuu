fb6868636274adaddcbad540233e17fa9de4d3fe
Lucakuuuu <163463371+Lucakuuuu@users.noreply.github.com>
2024-04-25 22:00:14 +0000

    new try

--- hashmap.c
+++ hashmap.c
@@ -134,7 +134,7 @@ Pair * nextMap(HashMap * map)
   while(posicion < map->capacity)
     {
       if(map->buckets[posicion]!=NULL && map->buckets[posicion]->key!=NULL) 
-        return map->buckets[posicion];
+        return map->buckets[posicion]->value;
       posicion = (posicion + 1) / map->capacity;
     }
   return NULL;
