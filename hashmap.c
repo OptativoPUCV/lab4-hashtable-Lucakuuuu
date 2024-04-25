fdda2080c6102f1711fecdaf9a4306d2a734dfd5
Lucakuuuu <163463371+Lucakuuuu@users.noreply.github.com>
2024-04-25 21:50:00 +0000

    new try

--- hashmap.c
+++ hashmap.c
@@ -120,7 +120,11 @@ Pair * firstMap(HashMap * map)
       if(map->buckets[map->current]!=NULL && map->buckets[map->current]->key!=NULL)
         return map->buckets[map->current];
       else
-        map->current--;
+      {
+        map->current++;
+        map->current = map->current % map->capacity;
+      }
+        //map->current--;
     }
   return NULL;
 }
