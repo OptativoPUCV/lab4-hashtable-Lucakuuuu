a895d3fac08287aac711af81b8fdd380d74c0c56
Lucakuuuu <163463371+Lucakuuuu@users.noreply.github.com>
2024-04-25 21:55:43 +0000

    new try

--- hashmap.c
+++ hashmap.c
@@ -139,6 +139,12 @@ Pair * nextMap(HashMap * map)
         map->current = map->current % map->capacity;
         return map->buckets[map->current];
       }
+      else
+      {
+        map->current++;
+        map->current = map->current % map->capacity;
+      }
+      
     }
   return NULL;
 }
