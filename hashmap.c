1af7fa48a38fe84e7c373cdd06ce8806401d4c0e
Lucakuuuu <163463371+Lucakuuuu@users.noreply.github.com>
2024-04-26 02:20:08 +0000

    new try

--- hashmap.c
+++ hashmap.c
@@ -140,14 +140,15 @@ Pair * firstMap(HashMap * map)
   return NULL;
 }
 
-Pair *nextMap(HashMap * map) {
+Pair *nextMap(HashMap * map) 
+{
   if (map == NULL || map->current == -1) return NULL;
-  for (long i = map->current + 1; i < map->capacity; i++) {
-    if (map->buckets[i] != NULL && map->buckets[i]->key != NULL) {
+  for (long i = map->current + 1; i < map->capacity; i++) 
+    if (map->buckets[i] != NULL && map->buckets[i]->key != NULL) 
+    {
       map->current = i;
       return map->buckets[i];
      }
-  }
   map->current = -1;
   return NULL;
 }
